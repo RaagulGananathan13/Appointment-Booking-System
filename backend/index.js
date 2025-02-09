26a4e4ba8091dc6bc1752d44ceab9c0b67acf370
Raagul <IT22130020@my.sliit.lk>
2025-02-09 23:27:37 +0530

    Created Get and Post routes for users

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,49 @@
 import express from "express"
+import mysql from "mysql"
+
 
 const app = express()
+app.use(express.json());
+
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user: "root",
+    password: "password",
+    database: "test",
+});
+
+
+
+app.get("/", (req, res) => {
+    res.json("hello this is the backend");
+  });
+
+  app.get("/users", (req, res) => {
+    const q = "SELECT * FROM users";
+    db.query(q, (err, data) => {
+      if (err) {
+        console.log(err);
+        return res.json(err);
+      }
+      return res.json(data);
+    });
+  });
+
+  app.post("/users", (req, res) => {
+    const q = "INSERT INTO users(`username`, `phone`, `profilepicture`) VALUES (?)";
+  
+    const values = [
+        req.body.username,
+        req.body.phone,
+        req.body.profilepicture,
+      ];
+
+    db.query(q, [values], (err, data) => {
+        if (err) return res.send(err);
+        return res.json(data);
+      });
+    });
 
 app.listen(8800, () =>{
     console.log("Connectd to Backend!")
