5f8819b68a01c0a09c6b3965c392389e3024659a
Raagul <IT22130020@my.sliit.lk>
2025-02-10 12:10:37 +0530

    Client-side add users

--- client/src/pages/AddUsers.jsx
+++ client/src/pages/AddUsers.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const AddUsers = () => {
+    const [user, setUser] = useState({
+        username:"",
+        email:"",
+        phone:"",
+        profilepicture:"",
+    });
+
+    const handleChange = (e) =>{
+        setUser(Prev=>({...Prev, [e.target.name]: e.target.value}));
+    };
+
+    const handleClick = async e =>{
+        e.preventDefault()
+        try{
+            await axios.post("http://localhost:8800/users", user);
+        }catch(err){
+
+        }
+        
+    }
+
+    console.log(user)
   return (
-    <div>
-      Add
+    <div className='form'>
+      <h1>Add new User</h1>
+      <input type="text" placeholder='username' onChange={handleChange} name="username"/>
+      <input type="email" placeholder='email' onChange={handleChange} name="email"/>
+      <input type="tel" placeholder='phone' onChange={handleChange} name="phone"/>
+      <input type="text" placeholder='profilepicture' onChange={handleChange} name="profilepicture"/>
+
+      <button onClick = {handleClick} ></button>
     </div>
   )
 }
--- client/src/pages/Users.jsx
+++ client/src/pages/Users.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom';
+
 
 const Users = () => {
+    const [users, setUsers] = useState( [])
+
+    useEffect( ()=>{
+        const fetchAllUsers = async ()=>{
+            try{
+                const res = await axios.get("http://localhost:8800/users");
+                setUsers(res.data);
+            }catch(err){
+                console.log(err)
+            }
+        }
+        fetchAllUsers()
+    }, [])
+
+
   return (
     <div>
-      Users
+      <h1>All Users</h1>
+      <div className="Users"> 
+        {users.map(user=>(
+            <div className="User" key={user.id}>
+                { user.profilepicture && <img src={user.profilepicture} alt="" />}
+                <h2>{user.username}</h2>
+                <h3>{user.phone}</h3>
+            </div>    
+        ))}
+      </div>
+      <button>
+        <Link to="/add">Add New User</Link>
+      </button>
     </div>
   )
 }
